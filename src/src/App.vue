c4bb911e030c1c42ae77379e52fb2bcdf50d391c
neomn110 <neomn110@gmail.com>
2023-06-11 18:37:03 +0330

    feat: trying to mount top bar

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <TopBar/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <script setup>
 import TopBar from './components/TobBar.vue'
+export default {
+  components: {TopBar},
+}
 </script>
