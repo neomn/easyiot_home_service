83ae9303333575d865f2fdd392815e90b5d0e736
neomn110 <neomn110@gmail.com>
2023-06-13 12:39:05 +0330

    feat: send button text as prop

--- src/src/components/MyButton.vue
+++ src/src/components/MyButton.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
-    this is my button 
+  <div class="">
+    <h3>{{text}}</h3> 
   </div>
 </template>
 
 
 
 <script>
-
+  export default {
+    props: ['text'],
+  }
 </script>
