f52874c52ebcac24b16d5f8d2802b3b444e9d786
neomn110 <neomn110@gmail.com>
2023-06-13 13:25:16 +0330

    refactor: replace div with a button

--- src/src/components/MyButton.vue
+++ src/src/components/MyButton.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex justify-center items-center h-12 mx-1 md:mx-4 px-4 border border-lime-300 text-sm text-gray-300 rounded-lg 
+  <button class="flex justify-center items-center h-12 mx-1 md:mx-4 px-4 border border-lime-300 text-sm md:text-lg text-gray-300 rounded-lg 
               overflow-hidden">
-    <h3>{{text}}</h3> 
-  </div>
+    {{text}}
+  </button>
 </template>
 
 
