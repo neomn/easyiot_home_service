9f284973df5b89766adb50f387144d204e428ba0
neomn110 <neomn110@gmail.com>
2023-06-15 20:57:48 +0330

    refactor: remove extra spacing

--- docker_cmd_script.sh
+++ docker_cmd_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
+ln -s /usr/bin/node-18 /usr/bin/node   
+ln -s /usr/bin/npm-18 /usr/bin/npm     
+ln -s /usr/bin/npx-18 /usr/bin/npx    
 nginx -g "daemon off;"
-ln -s /usr/bin/node-18  /usr/bin/node   
-ln -s /usr/bin/npm-18   /usr/bin/npm     
-ln -s /usr/bin/npx-18   /usr/bin/npx    
